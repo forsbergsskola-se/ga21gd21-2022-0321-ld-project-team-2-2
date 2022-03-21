3467f8c346a4d70598db8282f74ad23ff894a48e
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-03-21 15:29:15 +0100

    Movement + camera complete

--- Assets/Design/Elias/Scripts/PlayerMovement.cs
+++ Assets/Design/Elias/Scripts/PlayerMovement.cs
@@ -10,13 +10,57 @@ public class PlayerMovement : MonoBehaviour
     public float rotationSpeed;
     public float runSpeed;
 
+    [Header("Jump")] 
+    public float jumpSpeed;
+    public float gravityScale;
+    public float originalGravity;
+
+    [Header("Camera")] 
+    [SerializeField] private Transform cameraTransform;
+    
+    //Components
+    private CharacterController CharacterController;
+    private Animation animator;
+    
     private void Start()
     {
-        
+        CharacterController = GetComponent<CharacterController>();
+        originalGravity = gravityScale;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        Movement();
+        if (CharacterController.isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float magnitude = movementDirection.magnitude;
+        magnitude = Mathf.Clamp01(movementDirection.magnitude) * moveSpeed;
+        movementDirection = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) * movementDirection;
+        movementDirection.Normalize();
+
+        Vector3 velocity = movementDirection * magnitude;
+        CharacterController.Move(velocity * Time.deltaTime);
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation =
+                Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void Jump()
     {
         
     }
