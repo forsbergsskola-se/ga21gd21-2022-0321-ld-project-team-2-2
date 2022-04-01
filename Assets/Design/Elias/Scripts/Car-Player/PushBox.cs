1857a6835e4ffe42fd0772c2e5324343b5ccdfe8
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-04-01 16:51:51 +0200

    Started on push block script

--- Assets/Design/Elias/Scripts/Car-Player/PushBox.cs
+++ Assets/Design/Elias/Scripts/Car-Player/PushBox.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PushBox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float forceMagnitude;
 
-    // Update is called once per frame
-    void Update()
+    private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        
+        Rigidbody rigidbody = hit.collider.attachedRigidbody;
+        if (rigidbody != null)
+        {
+            Vector3 forceDirection = hit.gameObject.transform.position - transform.position;
+            forceDirection.y = 0;
+            forceDirection.Normalize();
+            
+            rigidbody.AddForceAtPosition(forceDirection * forceMagnitude, transform.position, ForceMode.Impulse);
+        }
     }
 }
