b029cfe3b3dd04f9f30cecd20d0fbc0a2043b31d
TomOlovsson <tom.olovsson@student.forsbergsskola.se>
2022-04-04 16:33:38 +0200

    Made some sound script templates
    - Started to make a teleporter script
    - Made a PlaySound script template

---
+++ Assets/Sound/Scripts/PlaySoundTemplate.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public FMODUnity.EventReference placeEventHere;
+    private FMOD.Studio.EventInstance myInstance;
+    public bool is3D = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInstance = FMODUnity.RuntimeManager.CreateInstance(placeEventHere);
+        if (is3D) FMODUnity.RuntimeManager.AttachInstanceToGameObject(myInstance, GetComponent<Transform>(), GetComponent<Rigidbody>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*To play event once and then scrap it:
+        if (enter what triggers the event) myInstance.start();
+
+        To stop the event:
+        if (enter what triggers the event stop) myInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+
+        Release the event:
+        if (enter what releases the event) myIstance.release();
+
+        Create temporary instances on the fly:
+        if (enter what triggers the event) 
+        {
+        FMOD.Studio.EventInstance.tempIns = FMODUnity.RuntimeManager.CreateInstance(placeEventHere);
+        tempInst.start();
+        tempInst.release();
+        
+        }
+        */
+    }
+}
---
+++ Assets/Sound/Scripts/Teleporter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    //Jag behöver fixa en funktion för att sätta en temporär parameter så en one-shot kan komma utöver loop-eventet när spelaren teleporteras.
+
+    public FMODUnity.EventReference placeEventHere;
+    private FMOD.Studio.EventInstance myInstance;
+    public bool is3D = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(myInstance, GetComponent<Transform>(), GetComponent<Rigidbody>());
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        myInstance = FMODUnity.RuntimeManager.CreateInstance(placeEventHere);
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(myInstance, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        myInstance.start();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        myInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        myInstance.release();
+    }
+
+}
