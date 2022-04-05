f770036fd087977a1e1ca7275119ca30ae935509
TomOlovsson <tom.olovsson@student.forsbergsskola.se>
2022-04-05 16:35:26 +0200

    Made a footsteps script

---
+++ Assets/Sound/Scripts/Footsteps.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    public FMODUnity.EventReference placeEventHere;
+    private FMOD.Studio.EventInstance myInstance;
+    public bool is3D = false;
+    void Start()
+    {
+        myInstance = FMODUnity.RuntimeManager.CreateInstance(placeEventHere);
+        if (is3D) FMODUnity.RuntimeManager.AttachInstanceToGameObject(myInstance, GetComponent<Transform>(), GetComponent<Rigidbody>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(is3D) FMODUnity.RuntimeManager.AttachInstanceToGameObject(myInstance, GetComponent<Transform>(), GetComponent<Rigidbody>());
+    }
+    public void Move()
+    {
+        myInstance.start();
+    }
+    public void StopMoving()
+    {
+        myInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
