f99d7f8bc20fac83f059d4daf11e39817e9f3877
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-04-27 10:46:06 +0200

    Buy menu open functionality

---
+++ Assets/Design/Elias/Scripts/Systems/Upgrade+Currency/OpenUpgradeScreen.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenUpgradeScreen : MonoBehaviour
+{
+    public KeyCode interact = KeyCode.E;
+    [SerializeField] private PlayerMovement controller;
+
+    [Header("Cameras")] 
+    public GameObject playerCamera;
+    public GameObject inventoryCamera;
+    
+    [Header("UI")] 
+    public GameObject ui;
+    
+    private bool inMenu = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UIFollow();
+        if (Input.GetKeyDown(interact))
+        {
+            if (inMenu)
+            {
+                controller.enabled = true;
+                playerCamera.SetActive(true);
+                inventoryCamera.SetActive(false);
+                
+                ui.SetActive(false);
+            }
+            else
+            {
+                controller.enabled = false;
+                inventoryCamera.SetActive(true);
+                playerCamera.SetActive(false);
+                //Enable mouse
+                
+                ui.SetActive(true);
+            }
+            inMenu = !inMenu;
+        }
+    }
+
+    void UIFollow()
+    {
+        //ui.transform.LookAt(inventoryCamera.transform);
+        //ui.transform.Rotate(0, 0, 0);
+    }
+}
