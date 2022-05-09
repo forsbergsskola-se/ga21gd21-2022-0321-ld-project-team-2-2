76ad779a2faac60359a4ebdf397c51d30a68764c
SearWaizy <sear.waizy@student.forsbergsskola.se>
2022-05-09 10:27:29 +0200

    Merge branch 'main' of https://github.com/forsbergsskola-se/ga21gd21-2022-0321-ld-project-team-2-2

--- Assets/Design/Elias/Scripts/Car-Player/PlayerMovement.cs
+++ Assets/Design/Elias/Scripts/Car-Player/PlayerMovement.cs
@@ -32,7 +32,7 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         animator = GetComponent<Animator>();
-        //Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
         CharacterController = GetComponent<CharacterController>();
         originalStepOffset = CharacterController.stepOffset;
     }
