f10a0423543ffde0db18858edf602548d1304955
Elias Bernmarker <elias.bernmarker@hotmail.com>
2022-05-10 15:09:28 +0200

    KeyUI script

--- Assets/Final/Scripts/Systems/Keys/KeyUI.cs
+++ Assets/Final/Scripts/Systems/Keys/KeyUI.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class KeyUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private bool keyValley;
+    [SerializeField] private bool keyMountain;
+    [SerializeField] private bool keyRuins;
+    [SerializeField] private bool keyCrevice;
+    [SerializeField] private bool keyToe;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private GameObject textOne;
+    [SerializeField] private GameObject textOneDone;
+    [SerializeField] private GameObject textTwo;
+    [SerializeField] private GameObject textTwoDone;
+    [SerializeField] private GameObject textThree;
+    [SerializeField] private GameObject textThreeDone;
+    [SerializeField] private GameObject textFour;
+    [SerializeField] private GameObject textFourDone;
+    [SerializeField] private GameObject textFive;
+    [SerializeField] private GameObject textFiveDone;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (keyValley)
+        {
+            textOne.SetActive(false);
+            textOneDone.SetActive(true);
+        }
+        else if (keyMountain)
+        {
+            textTwo.SetActive(false);
+            textTwoDone.SetActive(true);
+        }
+        else if (keyRuins)
+        {
+            textThree.SetActive(false);
+            textThreeDone.SetActive(true);
+        }
+        else if (keyCrevice)
+        {
+            textFour.SetActive(false);
+            textFourDone.SetActive(true);
+        }
+        else if (keyToe)
+        {
+            textFive.SetActive(false);
+            textFiveDone.SetActive(true);
+        }
     }
 }
